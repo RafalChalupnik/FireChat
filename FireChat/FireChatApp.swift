c27a0086696ec24d58061c3398081f2a937d5794
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2021-09-26 12:50:50 +0200

    Configured Firebase

--- FireChat/FireChatApp.swift
+++ FireChat/FireChatApp.swift
@@ -5,10 +5,15 @@
 //  Created by Rafał Chałupnik on 26/09/2021.
 //
 
+import Firebase
 import SwiftUI
 
 @main
 struct FireChatApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
